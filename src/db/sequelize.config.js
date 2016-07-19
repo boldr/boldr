1624a165e67e0c970cf2886cf1a99e29533e19e2
Steven Truesdell <steven@strues.io>
2016-07-19 10:08:42 -0600

    testing environments running in docker containers.

--- src/db/sequelize.config.js
+++ src/db/sequelize.config.js
@@ -8,19 +8,11 @@ module.exports = {
     host: config.db.host || 'localhost',
     dialect: 'postgres'
   },
-  // test: {
-  //   use_env_variable: 'DATABASE_URL',
-  //   username: config.db.user || 'ubuntu',
-  //   password: config.'',
-  //   database: 'circle_test',
-  //   host: '127.0.0.1',
-  //   dialect: 'postgres'
-  // },
   testing: {
     use_env_variable: 'DATABASE_URL',
-    username: config.db.user || 'ubuntu',
-    password: config.db.password || '',
-    database: config.db.name || 'circle_test',
+    username: config.db.user || 'postgres',
+    password: config.db.password || 'password',
+    database: config.db.name || 'postgres',
     host: '127.0.0.1',
     dialect: 'postgres'
   },
