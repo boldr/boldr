60e98a3bc01bd8c593f0384bfc7b715200edb8f1
Steven Truesdell <steven@strues.io>
2016-08-06 11:51:45 -0600

    update deps and babel config for new es2015

--- cms/tools/webpack/loader/babel.config.js
+++ cms/tools/webpack/loader/babel.config.js
@@ -4,7 +4,7 @@ const BABELQUERY = {
   // Do not include superfluous whitespace characters and line terminators.
   // When set to "auto" compact is set to true on input sizes of >100KB.
   compact: 'auto',
-  presets: ['react-hmre', 'react', 'es2015-webpack', 'stage-0'],
+  presets: ['react-hmre', 'react', ['es2015', { modules: 'commonjs'}], 'stage-0'],
   plugins: [['transform-runtime', { polyfill: true, regenerator: false }],
     'react-hot-loader/babel', 'transform-decorators-legacy']
 };
