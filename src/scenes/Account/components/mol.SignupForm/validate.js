ac5672068b4b8afe56cdc8640f92db6adc031a71
Steven Truesdell <steven@strues.io>
2016-08-28 17:53:53 -0600

    refactor(global): finishes switching from jsx to js extensions

--- src/scenes/Account/components/mol.SignupForm/validate.js
+++ src/scenes/Account/components/mol.SignupForm/validate.js
@@ -1,17 +1,17 @@
 const validate = values => {
   // IMPORTANT: values is an Immutable.Map here!
   const errors = {};
-  if (!values.displayName) {
-    errors.username = 'Required';
-  } else if (values.displayName.length > 30) {
+  if (!values.get('displayName')) {
+    errors.displayName = 'Required';
+  } else if (values.get('displayName').length > 30) {
     errors.displayName = 'Must be 30 characters or less';
   }
-  if (!values.email) {
+  if (!values.get('email')) {
     errors.email = 'Required';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.get('email'))) {
     errors.email = 'Invalid email address';
   }
-  if (!values.firstName) {
+  if (!values.get('firstName')) {
     errors.firstName = 'Required';
   }
   return errors;
